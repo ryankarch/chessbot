ff11ddac3d9976b407a13c6bc6441cd0cf1303f1
ryankarch <karchryan@gmail.com>
2023-01-12 11:15:02 -0500

    board coordinate to tuple conversion

--- game.py
+++ game.py
@@ -6,7 +6,9 @@ class Board(object):
         self.rules = {}
         self.board = []
 
-    def fen_to_board(self):
+        self.fen_to_board()
+
+    def convert_fen_to_board(self):
 
         self.board = []
         string = self.fen.split()
@@ -27,14 +29,28 @@ class Board(object):
         self.rules["castle"] = set(list(rules[1]))
         self.rules["enpassant"] = rules[2]
 
-    def board_to_fen(self):
+        # print(self.fen)
+        # print(self.board)
+        # print(self.rules)
+
+    def convert_board_to_fen(self):
         pass
     # populate 2d list using fen string
 
-    def get_valid_moves(board: list[list]):
+    def get_valid_moves(self, piece: str):
         pass
+
+def get_cell_tuple(cell: str):
+        # a b c d e f g h
+
+        # 0 1 2 3 4 5 6 7
+
+    return 8 - int(cell[1]), ord(cell[0]) - 97
+
 
 
 if __name__ == "__main__":
     b = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    b.convert_fen_to_board()
+    print(get_cell_tuple("h1"))
 
