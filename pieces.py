95ccd77e5aafd5d0bb5a5f458a19ad944851c572
thedomino1313 <dominic.c.beyer@gmail.com>
2023-03-27 12:01:53 -0400

    Removed anti-king logic

--- pieces.py
+++ pieces.py
@@ -11,7 +11,7 @@ class Piece(object):
     def clean_moves(self):
         self.moves = [pos for pos in self.moves if pos[0] >= 0 and pos[0] <= 7 and pos[1] >= 0 and pos[1] <= 7]
 
-class Pawn(Piece):    
+class Pawn(Piece):
     def get_moves(self, board):
         move_dir = 1 if self.color == 'w' else -1
         i, j = self.pos
@@ -19,21 +19,21 @@ class Pawn(Piece):
             self.moves.append((i-(1*move_dir), j))
             if isinstance(board[i-(2*move_dir)][j], Blank) and (i == 6 and self.color == 'w' or i == 1 and self.color == 'b'):
                 self.moves.append((i-(2*move_dir), j))
-        if not isinstance(board[i-(1*move_dir)][j-1], Blank) and board[i-(1*move_dir)][j-1].color != self.color and not isinstance(board[i-(1*move_dir)][j-1], King):
+        if not isinstance(board[i-(1*move_dir)][j-1], Blank) and board[i-(1*move_dir)][j-1].color != self.color:
             self.moves.append((i-(1*move_dir), j-1))
-        if not isinstance(board[i-(1*move_dir)][j+1], Blank) and board[i-(1*move_dir)][j+1].color != self.color and not isinstance(board[i-(1*move_dir)][j+1], King):
+        if not isinstance(board[i-(1*move_dir)][j+1], Blank) and board[i-(1*move_dir)][j+1].color != self.color:
             self.moves.append((i-(1*move_dir), j+1))
         
         self.clean_moves()
         
 
-class Rook(Piece):    
+class Rook(Piece):
     def get_moves(self, board):
         i, j = self.pos
         for i_ in range(i+1, 8):
             if isinstance(board[i_][j], Blank):
                 self.moves.append((i_, j))
-            elif board[i_][j].color != self.color and not isinstance(board[i_][j], King):
+            elif board[i_][j].color != self.color:
                 self.moves.append((i_, j))
                 break
             else:
@@ -41,7 +41,7 @@ class Rook(Piece):
         for i_ in range(i-1, 0, -1):
             if isinstance(board[i_][j], Blank):
                 self.moves.append((i_, j))
-            elif board[i_][j].color != self.color and not isinstance(board[i_][j], King):
+            elif board[i_][j].color != self.color:
                 self.moves.append((i_, j))
                 break
             else:
@@ -49,7 +49,7 @@ class Rook(Piece):
         for j_ in range(j+1, 8):
             if isinstance(board[i][j_], Blank):
                 self.moves.append((i, j_))
-            elif board[i][j_].color != self.color and not isinstance(board[i][j_], King):
+            elif board[i][j_].color != self.color:
                 self.moves.append((i, j_))
                 break
             else:
@@ -57,20 +57,20 @@ class Rook(Piece):
         for j_ in range(j-1, 0, -1):
             if isinstance(board[i][j_], Blank):
                 self.moves.append((i, j_))
-            elif board[i][j_].color != self.color and not isinstance(board[i][j_], King):
+            elif board[i][j_].color != self.color:
                 self.moves.append((i, j_))
                 break
             else:
                 break
 
 
-class Bishop(Piece):    
+class Bishop(Piece):
     def get_moves(self, board):
         i, j = self.pos
         for inc in range(1, min(8-i, 8-j)):
             if isinstance(board[i+inc][j+inc], Blank):
                 self.moves.append((i+inc, j+inc))
-            elif board[i+inc][j+inc].color != self.color and not isinstance(board[i+inc][j+inc], King):
+            elif board[i+inc][j+inc].color != self.color:
                 self.moves.append((i+inc, j+inc))
                 break
             else:
@@ -78,7 +78,7 @@ class Bishop(Piece):
         for inc in range(1, min(8-i, j)):
             if isinstance(board[i+inc][j-inc], Blank):
                 self.moves.append((i+inc, j-inc))
-            elif board[i+inc][j-inc].color != self.color and not isinstance(board[i+inc][j-inc], King):
+            elif board[i+inc][j-inc].color != self.color:
                 self.moves.append((i+inc, j-inc))
                 break
             else:
@@ -86,7 +86,7 @@ class Bishop(Piece):
         for inc in range(1, min(i, 8-j)):
             if isinstance(board[i-inc][j+inc], Blank):
                 self.moves.append((i-inc, j+inc))
-            elif board[i-inc][j+inc].color != self.color and not isinstance(board[i-inc][j+inc], King):
+            elif board[i-inc][j+inc].color != self.color:
                 self.moves.append((i-inc, j+inc))
                 break
             else:
@@ -94,24 +94,24 @@ class Bishop(Piece):
         for inc in range(1, min(i, j)):
             if isinstance(board[i-inc][j-inc], Blank):
                 self.moves.append((i-inc, j-inc))
-            elif board[i-inc][j-inc].color != self.color and not isinstance(board[i-inc][j-inc], King):
+            elif board[i-inc][j-inc].color != self.color:
                 self.moves.append((i-inc, j-inc))
                 break
             else:
                 break
 
 
-class Knight(Piece):    
+class Knight(Piece):
     def get_moves(self, board):
         pass
 
 
-class Queen(Piece):    
+class Queen(Piece):
     def get_moves(self, board):
         pass
 
 
-class King(Piece):    
+class King(Piece):
     def get_moves(self, board):
         pass
 
